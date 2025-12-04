1e91c1d293041088b3e47873e52b86863afe7ef3
Lance Cruz <G00428876@atu.ie>
2025-12-04 17:18:09 +0000

    Initial commit

---
+++ src/main/java/ie/atu/proxyservice/Week11ProxyServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.proxyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week11ProxyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week11ProxyServiceApplication.class, args);
+    }
+
+}
